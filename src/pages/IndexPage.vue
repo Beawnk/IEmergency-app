868dd9e0fa34e46acbe11c09a3fd2e86c11e6ba4
Bianca <bsineleck@hotmail.com>
2022-09-28 19:08:06 -0300

    Colocando cards na home

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,31 @@
 <template>
-  <ul class="emergency-list">
-    <li class="emergency">
-      <h4>Emergencia</h4>
-      <img />
+  <ul class="complaint-list">
+    <li class="complaints">
+      <h4>Aglomeração durante a pandemia</h4>
+      <img src="..//assets/coronavirus.png" />
+    </li>
+    <li class="complaints">
+      <h4>Violência doméstica</h4>
+      <img src="..//assets/danger.png" />
+    </li>
+    <li class="complaints">
+      <h4>Aglomeração durante a pandemia</h4>
+      <img src="..//assets/danger.png" />
+    </li>
+    <li class="complaints">
+      <h4>Incêndio</h4>
+      <img src="..//assets/danger.png" />
+    </li>
+    <li class="complaints">
+      <h4>Aglomeração durante a pandemia</h4>
+      <img src="..//assets/danger.png" />
+    </li>
+    <li class="complaints">
+      <h4>Aglomeração durante a pandemia</h4>
+      <img src="..//assets/danger.png" />
     </li>
   </ul>
 </template>
-
 <script>
 import { defineComponent } from "vue";
 
@@ -16,8 +35,33 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.emergency-list {
-  padding: 10px;
-  height: 100%;
+.complaint-list {
+  padding: 0px;
+  height: 85%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+
+  .complaints {
+    margin: 10px;
+    width: 180px;
+    height: 130px;
+    background-color: grey;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+    padding: 10px;
+
+    img {
+      width: 50px;
+    }
+    h4 {
+      font-size: 12px;
+      line-height: 16px;
+    }
+  }
 }
 </style>
